c81e2166818aac91638986bd629a2155148b01c8
Susmitha <108534777+Susmitha1502@users.noreply.github.com>
2023-06-25 17:04:45 +0530

    Update TrainList.js

--- Train Schedule Application/src/components/TrainList.js
+++ Train Schedule Application/src/components/TrainList.js
@@ -1,41 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, List, ListItem, Typography } from '@mui/material';
-import TrainDetails from './TrainDetails';
 
 function TrainList() {
-    const [trains, setTrains] = useState([]); // Initialize trains as an empty array
-  
-    useEffect(() => {
-      // Fetch the train data from the API
-      fetch("http://104.211.219.98:80/train/trains")
-        .then((response) => response.json())
-        .then((data) => setTrains(data))
-        .catch((error) => console.log(error));
-    }, []);
-  
-    // Check if trains is an array before using map
-    if (!Array.isArray(trains)) {
-      return <p>Loading...</p>;
-    }
-  
-    return (
-      <div>
+  const [trains, setTrains] = useState([]);
+
+  useEffect(() => {
+    // Fetch the train data from the API
+    fetch("http://104.211.219.98:80/train/trains")
+      .then((response) => response.json())
+      .then((data) => setTrains(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (!Array.isArray(trains)) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h1>All Trains</h1>
+      <ul>
         {trains.map((train) => (
-          <Train key={train.trainNumber} train={train} />
+          <li key={train.id}>
+            <Link to={`/trains/${train.trainNumber}`}>{train.trainName}</Link>
+          </li>
         ))}
-      </div>
-    );
-  }
-  
-  function Train({ train }) {
-    return (
-      <div>
-        <h2>{train.trainName}</h2>
-        <p>Train Number: {train.trainNumber}</p>
-        {/* Display other train details */}
-      </div>
-    );
-  }
-  
+      </ul>
+    </div>
+  );
+}
+
 export default TrainList;
+
+
