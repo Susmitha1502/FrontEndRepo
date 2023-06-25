d135f0ce303048d45b93fc2f47616cba54a30621
Susmitha <108534777+Susmitha1502@users.noreply.github.com>
2023-06-25 13:30:39 +0530

    Upload App.js

--- Train Schedule Application/App.js
+++ Train Schedule Application/App.js
@@ -10,32 +10,6 @@ function App() {
       <TrainList />
     </div>
   );
-  /*return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TrainList />} />
-        <Route path="/train/:id" element={<TrainDetails />} />
-      </Routes>
-    </Router>
-  );*/
 }
 
 export default App;
-
-/*import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AllTrainsPage from './pages/AllTrainsPage';
-import SingleTrainPage from './pages/SingleTrainPage';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AllTrainsPage />} />
-        <Route path="/train/:id" element={<SingleTrainPage />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;*/
