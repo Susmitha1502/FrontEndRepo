b96e5aaac12e331e42e8c73cf5a0c40c89d6814a
Susmitha <108534777+Susmitha1502@users.noreply.github.com>
2023-06-25 13:24:48 +0530

    Add files via upload

---
+++ Train Schedule Application/pages/AllTrainsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainList from '../components/TrainList';
+
+function AllTrainsPage() {
+  return (
+    <div>
+      <h1>All Trains</h1>
+      <TrainList />
+    </div>
+  );
+}
+
+export default AllTrainsPage;
